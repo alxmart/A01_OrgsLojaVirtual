5ae1f08507760630fa27fc90efa1724f761c2af6
alxmart <alxmartinez@gmail.com>
2024-06-05 16:06:26 -0300

    Re-editing MainActivity testing toast and view.

--- app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.luizafmartinez.a01_orgslojavirtual
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //Toast.makeText(this, "Bem vindo(a) ao Orgs", Toast.LENGTH_LONG).show()
+        //val view = View(this)
+        val view = TextView(this)
+        view.setText("Cesta de frutas")
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
     }
 }
+
+
