e6bce6b478fd11156943e608843b59257c367173
alxmart <alxmartinez@gmail.com>
2024-06-06 18:55:54 -0300

    Added class Produto (list) and starting Adapter config.(fun getItemCount)

---
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/model/Produto.kt
@@ -0,0 +1,11 @@
+package com.luizafmartinez.a01_orgslojavirtual.model
+
+import java.math.BigDecimal
+
+class Produto(
+    val nome: String,
+    val descricao: String,
+    val valor: BigDecimal
+) {
+
+}
--- app/src/main/java/com/luizafmartinez/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/luizafmartinez/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -2,19 +2,26 @@ package com.luizafmartinez.ui.recyclerview.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.luizafmartinez.a01_orgslojavirtual.model.Produto
 
-class ListaProdutosAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+class ListaProdutosAdapter(
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
-    }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+
     }
 
+    // override fun getItemCount(): Int = produtos.size
+    override fun getItemCount(): Int {
+        return produtos.size
+    }
 
 }
