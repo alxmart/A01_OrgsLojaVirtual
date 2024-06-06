06298988412074dd3b758bfe5de5b471284c4274
alxmart <alxmartinez@gmail.com>
2024-06-06 16:56:12 -0300

    Removing view access using findViewById

--- app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
@@ -14,14 +14,15 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nome = findViewById<TextView>(R.id.nome)
-        nome.text = "Cesta de frutas"
 
+
+
+        /*val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
         val descricao = findViewById<TextView>(R.id.descricao)
         descricao.text = "Laranja, manga e maçã"
-
         val valor = findViewById<TextView>(R.id.valor)
-        valor.text = "19.99"
+        valor.text = "19.99"*/
 
         //Toast.makeText(this, "Bem vindo(a) ao Orgs", Toast.LENGTH_LONG).show()
         //val view = View(this)
