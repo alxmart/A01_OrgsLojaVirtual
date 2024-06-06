28f3b23586528f766895737c5cf4209e59d02e59
alxmart <alxmartinez@gmail.com>
2024-06-06 16:46:54 -0300

    Configuring layout for Recycler View.

--- app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.luizafmartinez.a01_orgslojavirtual
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
@@ -8,9 +9,20 @@ import android.widget.Toast
 
 class MainActivity : Activity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
+
         //Toast.makeText(this, "Bem vindo(a) ao Orgs", Toast.LENGTH_LONG).show()
         //val view = View(this)
        /* val view = TextView(this)
