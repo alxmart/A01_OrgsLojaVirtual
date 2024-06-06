751fee5beb2a4c1855099ad14ea78e11e72b9664
alxmart <alxmartinez@gmail.com>
2024-06-06 18:17:50 -0300

    Configuration for Adapter.

---
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/ListaProdutosAdapter.kt
@@ -0,0 +1,7 @@
+package com.luizafmartinez.a01_orgslojavirtual
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListaProdutosAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+}
--- app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/a01_orgslojavirtual/MainActivity.kt
@@ -3,9 +3,9 @@ package com.luizafmartinez.a01_orgslojavirtual
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.luizafmartinez.a01_orgslojavirtual.R
+import com.luizafmartinez.a01_orgslojavirtual.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
@@ -14,6 +14,8 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
 
 
 
